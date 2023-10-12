e3433c5ed5c1b260d10f485e10631cbcd893670b
ZekromDaOne <rohan.nagashetti@gmail.com>
2023-10-11 19:35:40 -0700

    change TeamPropTest name

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeamPropTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeamPropTest.java
@@ -48,7 +48,7 @@ import java.util.List;
  * Use Android Studio to Copy this Class, and Paste it into your team's code folder with a new name.
  * Remove or comment out the @Disabled line to add this OpMode to the Driver Station OpMode list.
  */
-@TeleOp(name = "Team Prop Test", group = "Concept")
+@TeleOp(name = "Team Prop Sensing", group = "Test")
 public class TeamPropTest extends LinearOpMode {
 
     private static final boolean USE_WEBCAM = true;  // true for webcam, false for phone camera
