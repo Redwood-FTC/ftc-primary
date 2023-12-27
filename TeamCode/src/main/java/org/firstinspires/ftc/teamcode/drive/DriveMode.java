6d6e83d2d52ba3399d368b65a3f6d26b57229013
MaverickTopG <iris.teyssier@gmail.com>
2023-12-27 15:12:56 -0800

    condense multiline comments

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/DriveMode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/DriveMode.java
@@ -196,84 +196,51 @@ public class DriveMode extends OpMode {
     protected long timePixelModeChanged = System.currentTimeMillis();
 
     public class InputMapping {
-        /**
-         * Robot's forward-backward movement. Ranges from -1 to 1, inclusive.
-         */
+        /** Robot's forward-backward movement. Ranges from -1 to 1, inclusive. */
         public double axial = -gamepad1.left_stick_y; // Pushing stick forward gives negative value.
 
-        /**
-         * Robot's left-right movement. Ranges from -1 to 1, inclusive.
-         */
+        /** Robot's left-right movement. Ranges from -1 to 1, inclusive. */
         public double lateral = gamepad1.left_stick_x;
 
-        /**
-         * Robot's rotation movement. Ranges from -1 to 1, inclusive.
-         */
+        /** Robot's rotation movement. Ranges from -1 to 1, inclusive. */
         public double yaw = gamepad1.right_stick_x;
 
-        /**
-         * Rotate hook arm to hanging angle.
-         */
+        /** Rotate hook arm to hanging angle. */
         public boolean goToHangingAngle = gamepad2.dpad_up;
 
-        /**
-         * Rotate hook arm to drone launch angle.
-         */
+        /** Rotate hook arm to drone launch angle. */
         public boolean goToDroneAngle = gamepad2.dpad_down;
 
-        /**
-         * Wind in hanging winch.
-         */
+        /** Wind in hanging winch. */
         public boolean windWinch = gamepad2.dpad_right;
-        /**
-         * Unwind hanging winch.
-         */
+        /** Unwind hanging winch. */
         public boolean unwindWinch = gamepad2.dpad_left;
 
-        /**
-         * Raise the intake off of the floor.
-         */
+        /** Raise the intake off of the floor. */
         public boolean raiseIntake = gamepad2.a;
 
-        /**
-         * Release the hook.
-         */
+        /** Release the hook. */
         public boolean releaseHook = gamepad2.b;
 
-        /**
-         * Drop pixels from the bucket.
-         */
+        /** Drop pixels from the bucket. */
         public boolean dropBucketPixel = gamepad1.a;
 
-        /**
-         * Pause bucket wheel spinning
-         */
+        /** Pause bucket wheel spinning */
         public boolean pauseBucket = gamepad1.b;
 
-        /**
-         * Move pixel arm to drop position.
-         */
+        /** Move pixel arm to drop position. */
         public boolean goToDropPosition = gamepad1.right_trigger > 0.05;
 
-        /**
-         * Move pixel arm to load position.
-         */
+        /** Move pixel arm to load position. */
         public boolean goToLoadPosition = gamepad1.left_trigger > 0.05;
 
-        /**
-         * Lift the pixel arm.
-         */
+        /** Lift the pixel arm. */
         public boolean liftPixelArm = gamepad1.y;
 
-
-        /**
-         * Lower the pixel arm.
-         */
+        /** Lower the pixel arm. */
         public boolean lowerPixelArm = gamepad1.x;
 
-        /**
-         * Launch the drone.
-         */
+        /** Launch the drone. */
         public boolean launchDrone = gamepad2.right_bumper;
     }
 
@@ -388,7 +355,7 @@ public class DriveMode extends OpMode {
 
             long planeLaunched = -1;
             if (input.launchDrone) {
-                    if (planeLaunched == -1) {
+                if (planeLaunched == -1) {
                     launchServo.setPosition(1);
                     planeLaunched = System.currentTimeMillis();
                 } //after plane is launched, same button moves the servo back
